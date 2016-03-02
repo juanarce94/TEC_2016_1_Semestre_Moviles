cd013acbde6bfa8d7280fa21bdb81ef1be974932
juanmiguelarce94 <juanmiguelarce94@gmail.com>
2016-03-01 21:15:01 -0600

    Tarea1

---
+++ Tarea1/app/src/main/java/com/example/juan/tarea1/fragment1.java
@@ -0,0 +1,25 @@
+package com.example.juan.tarea1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Juan on 3/1/2016.
+ */
+public class fragment1 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view;
+
+        view = inflater.inflate(R.layout.fragment1, container);
+
+        return view;
+    }
+}
